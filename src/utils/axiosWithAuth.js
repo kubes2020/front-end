97261d1c35a5ac24479833c53b8f49ce93552738
A <jesus.e.estrada@hotmail.com>
2020-09-18 20:43:04 -0500

    CRA Boilerplate

---
+++ src/utils/axiosWithAuth.js
@@ -0,0 +1,11 @@
+import Axios from 'axios';
+
+export const axiosWithAuth = () => {
+  const token = window.localStorage.getItem('token');
+  return Axios.create({
+    headers: {
+      Authorization: token,
+    },
+    baseURL: 'http://localhost:3000',
+  });
+};
